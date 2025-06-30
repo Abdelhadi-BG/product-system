c725fcc6cbe19ca6dec200217af6b24581719922
Abdelhadi Bakhty <bakhtiazerreza2000@gmail.com>
2025-06-30 18:09:58 +0100

    fix: migrate to jakarta.* packages for Spring Boot 3

--- backend/src/main/java/com/example/productapp/model/Role.java
+++ backend/src/main/java/com/example/productapp/model/Role.java
@@ -3,7 +3,7 @@ package com.example.productapp.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "roles")
